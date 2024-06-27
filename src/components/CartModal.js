5823e6fa33e4f4ebbeed8e2048d2d3ddca15a57d
AYMEN GHERDAINE <gherdaineaymen1995@gmail.com>
2024-06-26 23:17:09 -0400

    implementing the checkout with stripe

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useCartStore } from "../hooks/useCartStore";
 import { media } from "@wix/sdk";
 import { useWixClient } from "../hooks/useWixClient";
+import CheckoutButton from "./CheckoutButton";
 
 const CartModal = () => {
   // Getting the wixClient instance from useWixClient hook
@@ -25,78 +26,74 @@ const CartModal = () => {
             {/* Cart Item */}
             {cart.lineItems.map((item) => {
               return (
-                <>
-                  <div className="flex gap-4" key={item._id}>
-                    {item.image && (
-                      <Image
-                        src={media.getScaledToFillImageUrl(
-                          item.image,
-                          100,
-                          100,
-                          {}
-                        )}
-                        alt="product in cart image"
-                        width={100}
-                        height={100}
-                        className="object-cover rounded-md"
-                      />
-                    )}
-                    <div className="flex flex-col gap-12 justify-between w-full">
-                      {/* Top */}
-                      <div className="">
-                        {/* Title */}
-                        <div className="flex items-center justify-between gap-8">
-                          <h3 className="font-semibold">
-                            {item.productName?.original}
-                          </h3>
-                          <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
-                            {item.quantity && item.quantity > 1 && (
-                              <div className="text-xs text-green-500">
-                                {item.quantity} x
-                              </div>
-                            )}{" "}
-                            ${item.price?.amount}
-                          </div>
-                        </div>
-                        {/* Description */}
-                        <div className="text-sm text-gray-500">
-                          {item.availability?.status}
+                <div className="flex gap-4" key={item._id}>
+                  {item.image && (
+                    <Image
+                      src={media.getScaledToFillImageUrl(
+                        item.image,
+                        100,
+                        100,
+                        {}
+                      )}
+                      alt="product in cart image"
+                      width={100}
+                      height={100}
+                      className="object-cover rounded-md"
+                    />
+                  )}
+                  <div className="flex flex-col gap-12 justify-between w-full">
+                    {/* Top */}
+                    <div className="">
+                      {/* Title */}
+                      <div className="flex items-center justify-between gap-8">
+                        <h3 className="font-semibold">
+                          {item.productName?.original}
+                        </h3>
+                        <div className="p-1 bg-gray-50 rounded-sm flex items-center gap-2">
+                          {item.quantity && item.quantity > 1 && (
+                            <div className="text-xs text-green-500">
+                              {item.quantity} x
+                            </div>
+                          )}{" "}
+                          ${item.price?.amount}
                         </div>
                       </div>
-                      {/* Bottom*/}
-                      <div className="w-full flex justify-between text-sm gap-14">
-                        <span className="text-gray-500">
-                          Qty. {item.quantity}
-                        </span>
-                        <span
-                          className="text-blue-500 cursor-pointer"
-                          onClick={() => removeItem(wixClient, item._id)}
-                        >
-                          Remove
-                        </span>
+                      {/* Description */}
+                      <div className="text-sm text-gray-500">
+                        {item.availability?.status}
                       </div>
                     </div>
-                  </div>
-                  <div className="">
-                    <div className="flex items-center justify-between font-semibold">
-                      <span className="">Subtotal</span>
-                      <span className="">${cart.subtotal?.amount}</span>
-                    </div>
-                    <p className="text-gray-500 text-sm mt-2 mb-2">
-                      Shipping and taxes calculated at checkout
-                    </p>
-                    <div className="flex justify-between text-sm mt-4">
-                      <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
-                        View Cart
-                      </button>
-                      <button className="rounded-md py-3 px-4 bg-black text-[#FBFBFB]">
-                        Checkout
-                      </button>
+                    {/* Bottom*/}
+                    <div className="w-full flex justify-between text-sm gap-14">
+                      <span className="text-gray-500">
+                        Qty. {item.quantity}
+                      </span>
+                      <span
+                        className="text-blue-500 cursor-pointer"
+                        onClick={() => removeItem(wixClient, item._id)}
+                      >
+                        Remove
+                      </span>
                     </div>
                   </div>
-                </>
+                </div>
               );
             })}
+            <div className="">
+              <div className="flex items-center justify-between font-semibold">
+                <span className="">Subtotal</span>
+                <span className="">${cart.subtotal?.amount}</span>
+              </div>
+              <p className="text-gray-500 text-sm mt-2 mb-2">
+                Shipping and taxes calculated at checkout
+              </p>
+              <div className="flex justify-between text-sm mt-4">
+                <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">
+                  View Cart
+                </button>
+                <CheckoutButton items={cart?.lineItems} />
+              </div>
+            </div>
           </div>
         </>
       )}
